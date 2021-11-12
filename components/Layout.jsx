6cab34a323a2e90a8315a64ab9318972157b227b
visotoniki9 <d.simon.ssx0@gmail.com>
2021-11-12 01:10:08 +0100

    Added basic layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,24 @@
 import Head from 'next/head';
 
-const Layout = function () {
+const Layout = function ({
+  title, keywords, description, children,
+}) {
   return (
     <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+      </Head>
+      {children}
     </div>
   );
+};
+
+Layout.defaultProps = {
+  title: 'DJ Events | Find the hottest parties',
+  description: 'Find the latest DJ and other musical events',
+  keywords: 'music, dj, edm',
 };
 
 export default Layout;
