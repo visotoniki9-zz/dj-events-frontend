50741819c883620741d45e8dace1bd108113edf5
visotoniki9 <d.simon.ssx0@gmail.com>
2021-11-16 23:26:18 +0100

    Fixed link color

--- pages/404.jsx
+++ pages/404.jsx
@@ -7,7 +7,9 @@ const NotFoundPage = function () {
       <div className="text-center">
         <h1 className="text-7xl">404</h1>
         <h4 className="font-semibold">Sorry, there is nothing here</h4>
-        <Link className="text-blue-300" href="/">Go back home</Link>
+        <p className="text-blue-400">
+          <Link href="/">Go back home</Link>
+        </p>
       </div>
     </Layout>
   );
