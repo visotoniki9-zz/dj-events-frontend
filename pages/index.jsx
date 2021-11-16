32dd3145adebaf4b1c7c7bfb75ccdbd2a35ba0a2
visotoniki9 <d.simon.ssx0@gmail.com>
2021-11-16 22:49:57 +0100

    home -> homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Layout from '../components/Layout';
 const Home = function () {
   return (
     <Layout>
-      <h1 className="font-poppins">Home</h1>
+      <h1 className="font-poppins">Home Page</h1>
     </Layout>
   );
 };
