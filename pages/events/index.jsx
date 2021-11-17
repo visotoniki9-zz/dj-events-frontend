b9a48fbc142cd8e3369ae3dec1ee44b7cb40b76c
visotoniki9 <d.simon.ssx0@gmail.com>
2021-11-17 23:32:09 +0100

    Added events page

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,11 +1,34 @@
+import { API_URL } from '../../config/index';
 import Layout from '../../components/Layout';
+import EventItem from '../../components/EventItem';
 
-const EventsIndexPage = function () {
+const Home = function ({ events }) {
   return (
     <Layout>
-      <h1>My events</h1>
+      <h1 className="text-3xl font-bold">Upcoming Events</h1>
+      {events.map(({
+        id, image, date, name, time, slug,
+      }) => (
+        <EventItem
+          key={id}
+          image={image}
+          date={date}
+          time={time}
+          name={name}
+          slug={slug}
+        />
+      ))}
     </Layout>
   );
 };
 
-export default EventsIndexPage;
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: events,
+    // revalidate: 1,
+  };
+}
+export default Home;
