c756ccc5f41c0338fcdee1e38fb2e9e6558d48d7
visotoniki9 <d.simon.ssx0@gmail.com>
2021-11-18 02:58:41 +0100

    Added event fetching

--- pages/events/[slug].jsx
+++ pages/events/[slug].jsx
@@ -1,11 +1,44 @@
-import Layout from '../../components/Layout';
+import Layout from '@components/Layout';
+import { API_URL } from '@config/index';
 
-const EventPage = function () {
+const EventPage = function ({ events }) {
+  console.log(events);
   return (
     <Layout>
       <h1>Event page</h1>
+      <h1>{events[0].name}</h1>
     </Layout>
   );
 };
 
 export default EventPage;
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const { events } = await res.json();
+
+  const paths = events.map((event) => ({
+    params: { slug: event.slug },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+  return {
+    props: { events },
+  };
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const res = await fetch(`${API_URL}/api/events/${slug}`);
+//   const events = await res.json();
+//   return {
+//     props: { events },
+//   };
+// }
