06db6ffff31a31752cf4d597a3a0042846d1d7b6
visotoniki9 <d.simon.ssx0@gmail.com>
2021-11-19 01:53:21 +0100

    Changed dev server backend port

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337';
 
 export default {
   API_URL,
