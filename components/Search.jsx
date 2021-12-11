88d6aec7f9f4973a939ad71ea6c7cbd41b2851e9
visotoniki9 <d.simon.ssx0@gmail.com>
2021-12-11 23:54:01 +0100

    Added more complete search

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
 const Search = function () {
+  const [term, setTerm] = useState('');
+  const router = useRouter();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push(`/events/search?term=${term}`);
+    setTerm('');
+  };
   return (
     <div>
-      <input
-        className="bg-white text-center border rounded-md py-1 outline-none focus:border-2 focus:border-red-400"
-        placeholder="Search Events"
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          className="bg-white text-center border rounded-md py-1 outline-none focus:border-2 focus:border-red-400"
+          placeholder="Search Events"
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </form>
     </div>
   );
 };
